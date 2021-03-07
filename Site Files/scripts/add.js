34b32993fb1b5764e2e276f2b2592da7eaa59384
JohnnySD <ba004629@bac.qld.edu.au>
2021-03-07 19:48:11 +1000

    Update

--- Site Files/scripts/add.js
+++ Site Files/scripts/add.js
@@ -1,4 +1,4 @@
-messages=["Item currently unavailable!","Sold out!","Error 404: Stock not found!","This one flew off the shelves!","More stock coming soon!","What do you think the boss would say of we told him, 'We have -1 of these left'?"]
+messages=["Item currently unavailable!","Sold out!","Error 404: Stock not found!","This one flew off the shelves!","More stock coming soon!","What do you think the boss would say of we told him, 'We have -1 of these left'?","Please come back later!"]
 function unavailable() {
 	alert(messages[~~(Math.random() * messages.length)]);
 }
