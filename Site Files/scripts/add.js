4205eef81d135ee4e7e989fa8b23fa72bbdc8dd3
JohnnySD <ba004629@bac.qld.edu.au>
2021-03-08 11:44:03 +1000

    Update

--- Site Files/scripts/add.js
+++ Site Files/scripts/add.js
@@ -1,1 +1,4 @@
-alert('Currently unavailable')
+messages=["Item currently unavailable!","Sold out!","Error 404: Stock not found!","This one flew off the shelves!","More stock coming soon!","What do you think the boss would say of we told him, 'We have -1 of these left'?","Please come back later!"]
+function unavailable() {
+	alert(messages[~~(Math.random() * messages.length)]);
+}
